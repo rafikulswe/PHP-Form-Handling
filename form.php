2f9e0ceca2a2e4ba774e9af6c9a60455370e9291
rafiswe <rafikulislam521@gmail.com>
2022-05-08 23:25:40 +0600

    lecture 10

--- form.php
+++ form.php
@@ -19,19 +19,76 @@
   </head>
   <body>
       <?php 
+        /**
+         * For Show validation message
+         *
+         * @param string $message
+         * @param string $alertType
+         */
+        function validate($message, $alertType="danger") {
+            return "<div class=\"alert alert-{$alertType} alert-dismissible fade show\" role=\"alert\">
+                {$message}!
+                <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>
+            </div>";
+        }
+
+        /**
+         * validateEmail For check valid email or not.
+         */
+        function validateEmail($email) {
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        /**
+         * filterEduMail for filtering institution email addresses
+         */
+        function filterEduMail($email) {
+            $validEmails = array('diu.edu.bd', 'brac.edu.bd', 'nsu.edu.bd');
+            $email_arr = explode('@', $email, 2);
+            if (in_array(end($email_arr), $validEmails)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
 
         if (isset($_POST['submitBtn'])) {
             $userName  = $_POST['userName'];
             $userEmail = $_POST['userEmail'];
             $phone     = $_POST['phone'];
 
-            if ($userName == '' || $userEmail == '' || $phone == '') {
-                echo "All fields are required";
+
+            if (empty($userName) || empty($userEmail) || empty($phone)) {
+
+                $validationMsg = validate('All fields are required');
+
+            } else if (validateEmail($userEmail) == false) {
+
+                $validationMsg = validate('Email is not valid', 'warning');
+
+            } else if (filterEduMail($userEmail) == false) {
+
+                $validationMsg = validate('Email is not a edu mail', 'warning');
+
             } else {
-                // echo "Everything is ok";
-                echo "<pre>";
-                print_r($_POST);
-                echo "</pre>";
+
+                $validationMsg = validate('Everything okay', 'success');
+
             }
             
         }
@@ -43,6 +100,14 @@
               <div class="card">
                     <div class="card-body">
                         <h1>Registration Form</h1>
+
+                        <?php 
+                            if (isset($validationMsg)) {
+                              echo $validationMsg;
+                            }
+                        ?>
+                        
+
                         <form method="POST" action="">
                             <div class="form-group">
                                 <label for="fieldthree" class="form-label">Name</label>
@@ -50,7 +115,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="fieldOne" class="form-label">Email address</label>
-                                <input type="email" name="userEmail" class="form-control" id="fieldOne">
+                                <input type="text" name="userEmail" class="form-control" id="fieldOne">
                             </div>
                             <div class="form-group">
                                 <label for="fieldFour" class="form-label">Phone</label>
